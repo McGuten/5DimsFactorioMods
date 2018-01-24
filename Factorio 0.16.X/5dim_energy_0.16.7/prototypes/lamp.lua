3ee42fbc90cf894eb57a26e1875dacb59abf2112
D10X1D0 <1463471+D10X1D0@users.noreply.github.com>
2018-01-24 18:50:48 +0100

    Added circuit network connection and coloring to the lamp and updated changelog
    http://steamcommunity.com/profiles/76561198018723931/screenshot/930425533012758610

--- Factorio 0.16.X/5dim_energy_0.16.7/prototypes/lamp.lua
+++ Factorio 0.16.X/5dim_energy_0.16.7/prototypes/lamp.lua
@@ -39,13 +39,17 @@ data:extend({
     corpse = "small-remnants",
     collision_box = {{-0.15, -0.15}, {0.15, 0.15}},
     selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
     energy_source =
     {
       type = "electric",
       usage_priority = "secondary-input"
     },
     energy_usage_per_tick = "5KW",
-    light = {intensity = 0.9, size = 200},
+    light = {intensity = 0.9, size = 200, color = {r=1.0, g=1.0, b=1.0}},
+    light_when_colored = {intensity = 0.9, size = 200, color = {r=1.0, g=1.0, b=1.0}},
+    glow_size = 200,
+    glow_color_intensity = 0.135,
     picture_off =
     {
       filename = "__5dim_energy__/graphics/icon/icon_5d_small-lamp_2.png",
@@ -62,6 +66,19 @@ data:extend({
       height = 75,
       x = 83,
       shift = {0, -0.1}
-    }
+    },
+    signal_to_color_mapping =
+    {
+      {type="virtual", name="signal-red", color={r=1,g=0,b=0}},
+      {type="virtual", name="signal-green", color={r=0,g=1,b=0}},
+      {type="virtual", name="signal-blue", color={r=0,g=0,b=1}},
+      {type="virtual", name="signal-yellow", color={r=1,g=1,b=0}},
+      {type="virtual", name="signal-pink", color={r=1,g=0,b=1}},
+      {type="virtual", name="signal-cyan", color={r=0,g=1,b=1}},
+    },
+
+    circuit_wire_connection_point = circuit_connector_definitions["lamp"].points,
+    circuit_connector_sprites = circuit_connector_definitions["lamp"].sprites,
+    circuit_wire_max_distance = default_circuit_wire_max_distance
   },
 })
